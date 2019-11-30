f5d5b7a6285ebf09e95e1537690f96fb5e59fe70
HyeonJoony <55316484+HyeonJoony@users.noreply.github.com>
2019-11-30 13:54:12 +0900

    Update colors.js

--- colors.js
+++ colors.js
@@ -21,11 +21,13 @@ function nightdayhandler(self){
         Body.setBackgroundColor('black');
         Body.setColor('white');
         self.value = 'day';
+	    
         Links.setColor('powderblue');                                            
     } else {   
         Body.setBackgroundColor('white');
         Body.setColor('black');
         self.value = 'night';
-	      Links.setColor('blue');					    
+	    
+	Links.setColor('blue');					    
     }
 }
